a7b16618ba662d519dc98362320333bb563280b8
MaxBePunkt <maximillianborret@gmail.com>
2022-02-14 14:48:19 +0100

    starting Home

--- nothingtotrash/src/pages/home/Home.js
+++ nothingtotrash/src/pages/home/Home.js
@@ -1,7 +1,22 @@
 const Home = () => {
     return (
         <section className="home-Sec">
-            <h1>home</h1>
+            <article className="header-Art">
+                <div>
+                    <h1>Hilf mit die Umwelt zu schützen</h1>
+                    <p>
+                        Abfälle bedrohen Vögel, Delfine und Co. Mehr als zehn
+                        Millionen Tonnen Abfälle gelangen jährlich in die
+                        Ozeane. Sie kosten Abertausende Meerestiere das Leben.
+                        Seevögel verwechseln Plastik mit natürlicher Nahrung,
+                        Delfine verfangen sich in alten Fischernetzen. Hilf mit
+                        Müll zu reduzieren und trashnothing.
+                    </p>
+                    <a className="btn-primary" href="/">
+                        Starte jetzt!
+                    </a>
+                </div>
+            </article>
         </section>
     );
 };
