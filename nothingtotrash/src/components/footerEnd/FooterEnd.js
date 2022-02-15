ec21dbd40cf1d9ebdb534298d34d5f2dde44c143
MaxBePunkt <maximillianborret@gmail.com>
2022-02-15 09:52:50 +0100

    added akhims branch

--- nothingtotrash/src/components/footerEnd/FooterEnd.js
+++ nothingtotrash/src/components/footerEnd/FooterEnd.js
@@ -2,12 +2,12 @@ import { NavLink, Link } from "react-router-dom";
 
 const FooterEnd = () => {
     return (
-        <div>
+        <div className="footerEnd">
             <a href="http://www.supercode.de" className="logo">
                 <img src="img/footer-section.png" alt="zu" className="last" />
             </a>
         </div>
     );
-}
+};
 
 export default FooterEnd;
