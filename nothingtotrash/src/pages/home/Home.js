16e011ea80887cc51f6648fa8cbdc677eb1a7118
MaxBePunkt <maximillianborret@gmail.com>
2022-02-15 09:38:53 +0100

    Homepage 90% ready

--- nothingtotrash/src/pages/home/Home.js
+++ nothingtotrash/src/pages/home/Home.js
@@ -17,6 +17,70 @@ const Home = () => {
                     </a>
                 </div>
             </article>
+            <article className="picsGrid-Art">
+                <h2>Lebe eCommerce mal anders</h2>
+                <div>
+                    <img src="img/gridImg1.svg" alt="gridImg1" />
+                    <h3>Verkaufen statt wegwerfen</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Risus faucibus egestas neque, quis nunc in turpis cursus
+                        eget.
+                    </p>
+                </div>
+                <div>
+                    <img src="img/gridImg2.svg" alt="gridImg1" />
+                    <h3>Verschenke und Schütze</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Risus faucibus egestas neque, quis nunc in turpis cursus
+                        eget.
+                    </p>
+                </div>
+                <div>
+                    <img src="img/gridImg3.svg" alt="gridImg1" />
+                    <h3>Der Umwelt zuliebe</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Risus faucibus egestas neque, quis nunc in turpis cursus
+                        eget.
+                    </p>
+                </div>
+            </article>
+            <article className="blueBack-Art">
+                <div className="blueBackFlex-Div">
+                    <img src="img/blueImg1.png" alt="blueImg1" />
+                    <div>
+                        <h3>Ohne Limits</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Risus faucibus egestas neque, quis nunc in
+                            turpis cursus eget. Lorem ipsum dolor sit amet,
+                            consectetur adipiscing elit. Risus faucibus egestas
+                            neque, quis nunc in turpis cursus eget.
+                        </p>
+                        <a className="btn-primary" href="/">
+                            Zur Dokumentation
+                        </a>
+                    </div>
+                </div>
+                <div className="blueBackFlex-Div">
+                    <div>
+                        <h3>Kenn deine Community</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Risus faucibus egestas neque, quis nunc in
+                            turpis cursus eget. Lorem ipsum dolor sit amet,
+                            consectetur adipiscing elit. Risus faucibus egestas
+                            neque, quis nunc in turpis cursus eget.
+                        </p>
+                        <a className="btn-primary" href="/">
+                            Zur Dokumentation
+                        </a>
+                    </div>
+                    <img src="img/blueImg2.png" alt="blueImg2" />
+                </div>
+            </article>
         </section>
     );
 };
