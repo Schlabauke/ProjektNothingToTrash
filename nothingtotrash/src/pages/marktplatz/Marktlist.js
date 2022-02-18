3fe0717e9785672ed3c2f6f67f8eca2039d2a8cc
Schlabauke <annabelle.jeffre@gmail.com>
2022-02-18 16:59:02 +0100

    images werden in die Datenbank hochgeladen, Backend dazu angepasst

--- nothingtotrash/src/pages/marktplatz/Marktlist.js
+++ nothingtotrash/src/pages/marktplatz/Marktlist.js
@@ -1,20 +1,20 @@
 import MarktplatzItem from "./MarktplatzItem";
 
 const Marktlist = (props) => {
+    console.log(props)
     return (
         <div className="marktlist">
             {props.data.map((e, i) => (
                 <MarktplatzItem
                     key={i}
                     id={e._id}
-                    title={e.title}
-                    description={e.description}
-                    img={e.img}
-                    price={e.price}
-                    status={e.status}
-                    brand={e.brand}
-                    shipping={e.shipping}
-                    quantity={e.quantity}
+                    title={e.Titel}
+                    description={e.Beschreibung}
+                    img={e.Bild.convertedImage}
+                    price={e.Preis}
+                    status={e.Zustand}
+                    shipping={e.Lieferung}
+                    quantity={e.Anzahl}
                 />
             ))}
         </div>
