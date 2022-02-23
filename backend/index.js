902c28ad703f035a6e188a3edc98909d8020ceab
MaxBePunkt <maximillianborret@gmail.com>
2022-02-23 16:36:43 +0100

    alles fertig bis jetzt

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,9 @@ const jwt = require("jsonwebtoken");
 const { registerUser } = require("./services/registerUser");
 const { LoginUser } = require("./services/loginUser");
 const {
-	addProduct,
-	getAllProducts,
-	findOneUser,
+    addProduct,
+    getAllProducts,
+    findOneUser,
 } = require("./db_access/user_dao");
 const { deleteFavorite } = require("./services/deleteFavorite");
 const { addFavorite } = require("./services/addFavoritetoUser");
@@ -28,104 +28,104 @@ app.use(express.urlencoded({ extended: false }));
 
 //POST-Routen
 app.post("/api/users/register", (req, res) => {
-	//Daten aus Inputfeld
-	const username = req.body.username;
-	const email = req.body.email;
-	const password = req.body.password;
+    //Daten aus Inputfeld
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
 
-	//registerUser
-	registerUser({ username, email, password })
-		.then(() => {
-			res.sendStatus(201, "User Created");
-		})
-		.catch(err => {
-			send("Sorry!But this User already exists!");
-		});
+    //registerUser
+    registerUser({ username, email, password })
+        .then(() => {
+            res.sendStatus(201, "User Created");
+        })
+        .catch((err) => {
+            send("Sorry!But this User already exists!");
+        });
 }); //funktioniert
 
 app.post("/api/users/login", (req, res) => {
-	//daten aus Input-Feldern
-	const email = req.body.email;
-	const password = req.body.password;
+    //daten aus Input-Feldern
+    const email = req.body.email;
+    const password = req.body.password;
 
-	LoginUser({ email, password })
-		.then(token => {
-			res.send(token);
-		})
-		.catch(err => {
-			console.log("Err bei LogIn", err);
-			res.send({ err: err.message });
-		});
+    LoginUser({ email, password })
+        .then((token) => {
+            res.send(token);
+        })
+        .catch((err) => {
+            console.log("Err bei LogIn", err);
+            res.send({ err: err.message });
+        });
 }); //funktioniert
 
 app.post("/api/products/addProduct/", verifyToken, (req, res) => {
-	let newProduct = req.body;
-	addProduct(newProduct)
-		.then(() => {
-			res.send({ productAdd: true });
-			console.log(newProduct);
-		})
-		.catch(err => {
-			console.log("Err in AddProduct", err);
-			res.send({ err: err.message });
-		});
+    let newProduct = req.body;
+    addProduct(newProduct)
+        .then(() => {
+            res.send({ productAdd: true });
+            console.log(newProduct);
+        })
+        .catch((err) => {
+            console.log("Err in AddProduct", err);
+            res.send({ err: err.message });
+        });
 }); //funktioniert
 
 app.post("/api/user/favorites", verifyToken, (req, res) => {
-	console.log("favoritesPostRoute", req.body);
-	const productObjId = req.body.productObjId;
-	const userObjId = req.body.userObjId;
-	console.log("productObjId:", productObjId);
-	addFavorite(userObjId, productObjId)
-		.then(userFavorite => {
-			res.status(201).send(userFavorite);
-		})
-		.catch(err => {
-			console.log(err, "Something went wrong at addFavorite");
-		});
+    console.log("favoritesPostRoute", req.body);
+    const productObjId = req.body.productObjId;
+    const userObjId = req.body.userObjId;
+    console.log("productObjId:", productObjId);
+    addFavorite(userObjId, productObjId)
+        .then((userFavorite) => {
+            res.status(201).send(userFavorite);
+        })
+        .catch((err) => {
+            console.log(err, "Something went wrong at addFavorite");
+        });
 });
 
 app.delete("/api/user/favorites", verifyToken, (req, res) => {
-	console.log("Delete Route");
-	const productObjId = req.body.productObjId;
-	const userObjId = req.body.userObjId;
-	console.log("delete Nudel:", userObjId, productObjId);
-	deleteFavorite(userObjId, productObjId)
-		.then(deleteFavorite => {
-			res.send(deleteFavorite);
-		})
-		.catch(err => {
-			console.log(err, "Something went wrong at deleteFavorite");
-		});
+    console.log("Delete Route");
+    const productObjId = req.body.productObjId;
+    const userObjId = req.body.userObjId;
+    console.log("delete Nudel:", userObjId, productObjId);
+    deleteFavorite(userObjId, productObjId)
+        .then((deleteFavorite) => {
+            res.send(deleteFavorite);
+        })
+        .catch((err) => {
+            console.log(err, "Something went wrong at deleteFavorite");
+        });
 });
 
 //GET-Routen
 
 //für den allgemeinen Marktplatz
 app.get("/api/products/allProducts", (req, res) => {
-	// console.log("davor");
-	getAllProducts()
-		.then(allProducts => {
-			// console.log(allProducts);
-			res.send(allProducts);
-		})
-		.catch(err => {
-			console.log("Err in GET/allProducts", err);
-			res.send({ err: err.message });
-		});
+    // console.log("davor");
+    getAllProducts()
+        .then((allProducts) => {
+            // console.log(allProducts);
+            res.send(allProducts);
+        })
+        .catch((err) => {
+            console.log("Err in GET/allProducts", err);
+            res.send({ err: err.message });
+        });
 });
 
 app.get("/api/user/favorites", verifyToken, (req, res) => {
-	const id = req.headers.userid;
-	console.log("UserId", id);
-	findOneUser(id)
-		.then(favorites => {
-			res.send(favorites);
-		})
-		.catch(err => {
-			console.log("Err in userFavorites", err);
-			res.send({ err: err.message });
-		});
+    const id = req.headers.userid;
+    console.log("UserId", id);
+    findOneUser(id)
+        .then((favorites) => {
+            res.send(favorites);
+        })
+        .catch((err) => {
+            console.log("Err in userFavorites", err);
+            res.send({ err: err.message });
+        });
 });
 
 const PORT = 3001;
