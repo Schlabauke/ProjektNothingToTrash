c682f7e60c24ef60d189c5c33b4acbd26ad7afd1
MaxBePunkt <maximillianborret@gmail.com>
2022-02-24 15:41:10 +0100

    final final

--- nothingtotrash/src/pages/ueberuns/Ueberuns.js
+++ nothingtotrash/src/pages/ueberuns/Ueberuns.js
@@ -6,144 +6,68 @@ const Ueberuns = () => {
         <>
             <section className="ueberuns-Sec">
                 <div className="content">
-                    <h1>Unsere Vision</h1>
-
+                    <h1>Unser Projekt</h1>
                     <article>
-                        <h4>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing
-                            elit. Tempore delectus illum pariatur unde
-                            praesentium ipsa. Lorem ipsum dolor sit amet
-                            consectetur adipisicing elit. Possimus, quia.
-                            Ducimus qui iste praesentium necessitatibus fuga
-                            repellat animi. Molestiae, omnis!
-                        </h4>
                         <p>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing
-                            elit. Tempore delectus illum pariatur unde
-                            praesentium ipsa. Lorem ipsum dolor sit amet
-                            consectetur adipisicing elit. Possimus, quia.
-                            Ducimus qui iste praesentium necessitatibus fuga
-                            repellat animi. Molestiae, omnis! Lorem ipsum dolor
-                            sit amet consectetur adipisicing elit. Possimus,
-                            quia. Ducimus qui iste praesentium necessitatibus
-                            fuga repellat animi. Molestiae, omnis! repellat
-                            animi. Molestiae, omnis! Lorem ipsum dolor sit amet
-                            consectetur adipisicing elit. Possimus, quia.
-                            Ducimus qui iste praesentium necessitatibus fuga
-                            repellat animi. Molestiae, omnis! repellat animi.
-                            Molestiae, omnis! Lorem ipsum dolor sit amet
-                            consectetur adipisicing elit. Possimus, quia.
-                            Ducimus qui iste praesentium necessitatibus fuga
-                            repellat animi. Molestiae, omnis!
+                            ”Nothingtotrash” ist eine Website um gebrauchte
+                            Gegenstände zu verschenken oder für wenig Geld zu
+                            Verkaufen. Somit können Nutzer nachhaltiger
+                            wirtschaften und noch funktionale Gegenstände
+                            weitergeben ohne dass sie unnötig zu Müll werden.
                         </p>
-                        <img
-                            src="https://cdn.pixabay.com/photo/2020/06/24/05/29/tree-5334822_1280.png"
-                            alt=""
-                        />
                     </article>
                 </div>
-                <div className="picture">
-                    <img
-                        src="https://cdn.pixabay.com/photo/2014/07/30/08/50/earth-405096_1280.jpg"
-                        alt=""
-                    />
-                </div>
-                <div className="team">
-                    <h2>NothingToTrash Team</h2>
-                </div>
-            </section>
 
+                <img
+                    src="https://cdn.pixabay.com/photo/2020/06/24/05/29/tree-5334822_1280.png"
+                    alt=""
+                />
+            </section>
             <section className="card">
+                <h2>NothingToTrash Team</h2>
                 <div className="player">
                     <img
-                        className="red"
                         src="https://www.pokedexia.net/images/3/34/PGL-007.png"
                         alt=""
                     />
-                    <h2>Annnabelle Backend Developerin</h2>
-                    <p>
-                        Schiggy ist ein kleines, zweibeiniges und
-                        schildkrötenähnlichesWikipedia-Icon Pokémon. Es trägt
-                        einen braunen Schildkrötenpanzer auf dem Rücken seines
-                        hellblau gefärbten Körper, mit einem beigen Bauchpanzer.
-                        Das Mittelstück, des mit schwarzen, dünnen Linien
-                        versehenen Panzers, ist weiß gehalten. Schiggys Kopf ist
-                        rundlicher Gestalt und mit zwei großen Augen versehen,
-                        die wiederum aus weißer Lederhaut, violetten Iriden und
-                        schwarzen, länglichen Pupillen bestehen. Sein Maul ist
-                        recht breit und die Oberlippe leicht eingekrümmt. Die
-                        Nasenlöcher sind ebenso wie die Augenbrauen nur leicht
-                        angedeutet. Außerdem besitzt Schiggy einen
-                        spiralförmigen Schweif. In der schillernden Form hat
-                        Schiggy einen grünen Rückenpanzer, der Bauchpanzer ist
-                        nun in einem gelblichen Ton gehalten und der Hautton hat
-                        zu einem deutlich hellerem Blau gewechselt.
-                    </p>
+                    <h2>Annabelle Backend Developerin</h2>
+                    <p>Git-Queen</p>
+                    <p>Heroku-Schildmaid</p>
+                    <p>Orga-Herrin</p>
+                    <p>Backend halt</p>
                 </div>
                 <div className="player ">
                     <img
-                        className="red"
-                        id="player1"
                         src="https://www.pokewiki.de/images/thumb/4/48/Sugimori_006m2.png/250px-Sugimori_006m2.png"
                         alt=""
                     />
-                    <h2>Akhim Frondend Developer</h2>
-                    <p>
-                        Glurak ist ein Pokémon mit den Typen Feuer und Flug und
-                        existiert seit der ersten Spielgeneration. Es stellt die
-                        zweite Entwicklungsstufe von Glumanda und Glutexo dar
-                        und ist somit neben Bisaflor und Turtok eine der
-                        Endentwicklungen der Starter-Pokémon aus Kanto. Seit der
-                        sechsten Generation ist es in der Lage, eine
-                        Mega-Entwicklung zu Mega-Glurak X oder Mega-Glurak Y zu
-                        vollführen, wobei sich bei der Mega-Entwicklung zu
-                        Mega-Glurak X sein Typ zu Feuer und Drache ändert;
-                        folglich ist es neben Mewtu das einzige Pokémon mit zwei
-                        verschiedenen Mega-Entwicklungen. Außerdem kann es seit
-                        der achten Generation per Gigadynamaximierung zu
-                        Gigadynamax-Glurak werden.
-                    </p>
+                    <h2>Akhim Frontend Developer</h2>
+                    <p>MediaQuerie-Master</p>
+                    <p>Styling</p>
+                    <p>Überuns</p>
+                    <p>Frontend halt</p>
                 </div>
                 <div className="player">
                     <img
-                        className="red"
                         src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/133.png"
                         alt=""
                     />
-                    <h2>Max Frondend Developer</h2>
-                    <p>
-                        Evoli ist ein Pokémon mit dem Typ Normal und existiert
-                        seit der ersten Spielgeneration. Es ist das Pokémon mit
-                        der am größten verzweigten Evolution und kann sich in
-                        acht verschiedene Pokémon weiterentwickeln, die
-                        Evolitionen genannt werden, wobei es bei der Entwicklung
-                        immer den Typ wechselt. Seit der achten Generation ist
-                        es in der Lage, per Gigadynamaximierung zu
-                        Gigadynamax-Evoli zu werden.
-                    </p>
+                    <h2>Max Frontend Developer</h2>
+                    <p>Chef vom Frontend</p>
+                    <p>Schnittstelle Frontend/Backend</p>
+                    <p>Ordnerstruktur</p>
+                    <p>Frontend halt</p>
                 </div>
                 <div className="player">
                     <img
-                        className="red"
                         src="https://www.pokedexia.net/images/9/9c/PGL-202.png"
                         alt=""
                     />
                     <h2>Semir Backend Developer</h2>
-                    <p>
-                        Woingenau ist ein mittelgroßes, vierbeiniges Pokémon,
-                        das himmelblau und schwarz erscheint. Bis auf den
-                        schwarzen Schwanz ist der gesamte Körper himmelblau
-                        gefärbt. Während der Kopf und der Oberkörper gleich
-                        breit sind, wird Woingenau von der Taille abwärts bis zu
-                        den Füßen immer schmaler. Der Hinterkopf wächst dabei
-                        etwas weiter nach unten. Das Gesicht des Psycho-Pokémon
-                        wird von dem großen Mund dominiert. Der Mund hat dabei
-                        ein Zick-Zack-Muster. Bei weiblichen Woingenau sind
-                        dabei deutlich rot gefärbte Lippen erkennbar. Die Augen
-                        hält es stets geschlossen. Die langen Arme wachsen
-                        seitlich des Brustbereiches und haben eine rechteckige
-                        Gestalt.
-                    </p>
+                    <p>Datenbank-Chef</p>
+                    <p>Schnittstelle Frontend/Backend</p>
+                    <p>Testing</p>
+                    <p>Backend halt</p>
                 </div>
             </section>
             <Footer />
